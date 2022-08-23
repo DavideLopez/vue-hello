81150f0f6928abb4d6f42ae6b2b065fa38b6cf30
DavideLopez <davidelopez7@gmail.com>
2022-08-23 14:17:52 +0200

    fine + bonus img

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('vue')
+
+const app = new Vue ({
+    el: '#root',
+    data: {
+        titolo: 'VUE HELLO',
+        image: 'https://picsum.photos/400/600',
+    },
+    methods: {
+        toUpper(text) {
+            return text.toUpperCase()
+        },
+    },
+}
+)
